bce9d398a184ee02fdf9c1a7ac6ba82b5efae6cb
emanuele <>
2008-07-31 15:53:03 +0000

    skimmer first commit

---
+++ src/Skimmer.cc
@@ -0,0 +1,36 @@
+#include <fstream.h>
+#include <iostream.h>
+
+#include "CommonTools/include/Skimmer.hh"
+
+Skimmer::Skimmer(const char *skimTxtFile) {
+  m_file = skimTxtFile;
+}
+
+Skimmer::~Skimmer() {}
+
+void Skimmer::readFile() {
+
+  ifstream skimFile(m_file);
+  while(!skimFile.eof()) {
+    int bit = -1;
+    skimFile >> bit;
+    bits.push_back(bit);
+  }
+
+}
+
+bool Skimmer::output(int event) {
+
+  if( bits.size() == 0 ) {
+    cout << "SKIMMER: probably you have not initialised correctly the skim from file..." << std::endl; 
+  }
+
+  if( event > bits.size() ) {
+    std::cout << "!!! Serious ERROR! The number of requested event is larger than the skim file!" << std::endl;
+    return 0;
+  } else {
+    return bits[event];
+  }
+
+}
